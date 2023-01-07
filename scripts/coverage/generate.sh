7fce11e6d3c63d9a033ef2d88ee6e7e2e5599c0c
vigenere23 <lolgab1@hotmail.com>
2023-01-07 16:22:14 -0500

    better code coverage

--- scripts/coverage/generate.sh
+++ scripts/coverage/generate.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function generate_coverage {
+    output_type=$1
+    output_path=$2
+
+    grcov ./target/coverage \
+        --binary-path ../target/debug \
+        -s . \
+        -t $output_type \
+        --ignore-not-existing \
+        -o $output_path
+}
+
 package=$1
 
 if [[ -z "$package" ]]; then
@@ -18,11 +30,10 @@ RUSTFLAGS="-C instrument-coverage" \
 LLVM_PROFILE_FILE="target/coverage/%p-%m.profraw" \
 cargo test
 
-grcov ./target/coverage \
-    --binary-path ../target/debug \
-    -s . \
-    -t html \
-    --ignore-not-existing \
-    -o ./target/coverage/html
+echo "Generating HTML coverage file..."
+generate_coverage html ./target/coverage/html
+echo "HTML Coverage file outputed to $package/target/coverage/html/index.html"
 
-echo "Coverage file outputed to $package/target/coverage/html/index.html"
+echo "Generating XML coverage file..."
+generate_coverage cobertura ./target/coverage/coverage.xml
+echo "Codecov Coverage file outputed to $package/target/coverage/coverage.xml"
