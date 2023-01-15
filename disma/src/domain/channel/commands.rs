17b0f33a1432e796dfc3450e069e2b38fa9f15e0
Gab S <32545895+vigenere23@users.noreply.github.com>
2023-01-15 17:22:01 -0500

    Fix: Keep extra channels in categories (#43)
    Closes #42

--- disma/src/domain/channel/commands.rs
+++ disma/src/domain/channel/commands.rs
@@ -55,21 +55,23 @@ impl CommandFactory for AwaitingChannelsList {
                 .as_ref()
                 .map(|category| category.name());
 
-            let awaiting_channel = self.items.find(
+            let matching_awaiting_channel = self.items.find(
                 &existing_channel.name,
                 existing_channel.channel_type(),
                 category_name,
             );
 
-            let extra_items_strategy = match awaiting_channel {
-                Some(channel) => match &channel.category {
-                    Some(category) => category.extra_channels_strategy.clone(),
-                    None => self.extra_items_strategy.clone(),
-                },
+            let matching_awaiting_category = existing_channel
+                .category_name()
+                .map(|category_name| self.categories.find_by_name(&category_name))
+                .unwrap_or_default();
+
+            let extra_items_strategy = match matching_awaiting_category {
+                Some(category) => category.extra_channels_strategy.clone(),
                 None => self.extra_items_strategy.clone(),
             };
 
-            if awaiting_channel.is_none() {
+            if matching_awaiting_channel.is_none() {
                 extra_items_strategy.handle_extra_channel(existing_channel, &mut commands);
             }
         }
