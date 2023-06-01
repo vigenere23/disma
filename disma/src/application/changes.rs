d086dd7fe23c06756ee42de441bdf2d052c18fd2
vigenere23 <lolgab1@hotmail.com>
2023-06-01 09:54:55 -0400

    chore: removing unused duplicated method

--- disma/src/application/changes.rs
+++ disma/src/application/changes.rs
@@ -45,26 +45,6 @@ impl ChangesService {
             .collect()
     }
 
-    pub fn list_changes_2(
-        &self,
-        guild_id: &str,
-        guild_params: GuildParams,
-    ) -> Vec<CommandDescription> {
-        let awaiting_guild: AwaitingGuild = guild_params.into();
-        let existing_guild = self.guild_querier.get_guild(guild_id);
-
-        let role_commands = awaiting_guild.roles.commands_for(&existing_guild);
-        let category_commands = awaiting_guild.categories.commands_for(&existing_guild);
-        let channel_commands = awaiting_guild.channels.commands_for(&existing_guild);
-
-        role_commands
-            .into_iter()
-            .chain(category_commands.into_iter())
-            .chain(channel_commands.into_iter())
-            .map(|command| command.describe())
-            .collect()
-    }
-
     pub fn apply_changes(&self, guild_id: &str, guild_params: GuildParams) {
         let awaiting_guild: AwaitingGuild = guild_params.into();
 
