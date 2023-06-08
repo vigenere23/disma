f7b7e7134cc1b7dc0e37ea2d4970c60082f9f7d2
vigenere23 <lolgab1@hotmail.com>
2023-06-08 07:51:03 -0400

    chore: moved tests to the right file

--- disma/src/domain/diff.rs
+++ disma/src/domain/diff.rs
@@ -78,3 +78,94 @@ where
 
     diffs
 }
+
+#[cfg(test)]
+mod tests {
+    mod vec_diffs {
+        use crate::diff::{Diff, Differ};
+
+        #[test]
+        fn it_calculates_additions() {
+            let origin = vec!["hello"];
+            let target = vec!["hello", "world!"];
+
+            let diffs = origin.diffs_with(&target);
+
+            let expected_diffs = vec![Diff::Add("world!".into())];
+            assert_eq!(diffs, expected_diffs);
+        }
+
+        #[test]
+        fn it_calculates_removals() {
+            let origin = vec!["hello", "world!"];
+            let target = vec!["hello"];
+
+            let diffs = origin.diffs_with(&target);
+
+            let expected_diffs = vec![Diff::Remove("world!".into())];
+            assert_eq!(diffs, expected_diffs);
+        }
+
+        #[test]
+        fn it_calculates_both_additions_and_removals() {
+            let origin = vec!["super", "mario"];
+            let target = vec!["hello", "world!"];
+
+            let diffs = origin.diffs_with(&target);
+
+            let expected_diffs = vec![
+                Diff::Remove("super".into()),
+                Diff::Remove("mario".into()),
+                Diff::Add("hello".into()),
+                Diff::Add("world!".into()),
+            ];
+            assert_eq!(diffs, expected_diffs);
+        }
+
+        #[test]
+        fn given_same_arrays_returns_no_diff() {
+            let origin = vec!["hello", "world!"];
+            let target = vec!["hello", "world!"];
+
+            let diffs = origin.diffs_with(&target);
+
+            assert_eq!(diffs.len(), 0);
+        }
+    }
+
+    mod str_diffs {
+        use crate::diff::{Diff, Differ};
+
+        #[test]
+        fn given_same_str_returns_no_diff() {
+            let origin = "hello";
+            let target = "hello";
+
+            let diffs = origin.diffs_with(&target);
+
+            assert_eq!(diffs.len(), 0);
+        }
+
+        #[test]
+        fn can_differ_strings() {
+            let origin = "hello";
+            let target = "world!";
+
+            let diffs = origin.diffs_with(&target);
+
+            let expected_diffs = vec![Diff::Remove("hello".into()), Diff::Add("world!".into())];
+            assert_eq!(diffs, expected_diffs);
+        }
+
+        #[test]
+        fn can_differ_bools() {
+            let origin = true;
+            let target = false;
+
+            let diffs = origin.diffs_with(&target);
+
+            let expected_diffs = vec![Diff::Remove("true".into()), Diff::Add("false".into())];
+            assert_eq!(diffs, expected_diffs);
+        }
+    }
+}
