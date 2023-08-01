cead16dc18631c73ee392a69768a751766075f13
vigenere23 <lolgab1@hotmail.com>
2023-08-01 08:25:47 -0400

    refactor: implemented comparison for roles lists

--- disma/src/domain/role/base.rs
+++ disma/src/domain/role/base.rs
@@ -12,6 +12,12 @@ where
     items: Vec<R>,
 }
 
+pub struct ListComparison<R1, R2> {
+    pub extra_self: Vec<R1>,
+    pub extra_other: Vec<R2>,
+    pub same: Vec<(R1, R2)>,
+}
+
 impl<R: Role> RolesList<R> {
     pub fn find_by_name(&self, name: &str) -> Option<&R> {
         self.items.iter().find(|role| role.name() == name)
@@ -19,6 +25,34 @@ impl<R: Role> RolesList<R> {
 
     pub fn to_list(&self) -> &Vec<R> {
         &self.items
+    }
+
+    pub fn compare_by_name<'a, R2: Role>(
+        &'a self,
+        other: &'a RolesList<R2>,
+    ) -> ListComparison<&R, &R2> {
+        let mut extra_self: Vec<&R> = Vec::new();
+        let mut extra_other: Vec<&R2> = Vec::new();
+        let mut same: Vec<(&R, &R2)> = Vec::new();
+
+        for self_item in self.to_list() {
+            match other.find_by_name(self_item.name()) {
+                Some(other_item) => same.push((self_item, other_item)),
+                None => extra_self.push(self_item),
+            }
+        }
+
+        for other_item in other.to_list() {
+            if self.find_by_name(other_item.name()).is_none() {
+                extra_other.push(other_item)
+            }
+        }
+
+        ListComparison {
+            extra_self,
+            extra_other,
+            same,
+        }
     }
 }
 
--- disma/src/domain/role/commands.rs
+++ disma/src/domain/role/commands.rs
@@ -7,7 +7,7 @@ use crate::{
     guild::{ExistingGuild, GuildCommanderRef},
 };
 
-use super::{AwaitingRole, AwaitingRolesList, ExistingRole};
+use super::{AwaitingRole, AwaitingRolesList, ExistingRole, ListComparison};
 
 pub trait ExtraRolesStrategyTrait {}
 
@@ -15,30 +15,29 @@ impl CommandFactory for AwaitingRolesList {
     fn commands_for(&self, existing_guild: &ExistingGuild) -> Vec<CommandRef> {
         let mut commands: Vec<CommandRef> = Vec::new();
 
-        for awaiting_role in self.items.to_list() {
-            match existing_guild.roles.find_by_name(&awaiting_role.name) {
-                Some(existing_role) => {
-                    if existing_role != awaiting_role {
-                        let command = UpdateRole::new(
-                            existing_role.clone(),
-                            awaiting_role.clone(),
-                            existing_role.diffs_with(awaiting_role),
-                        );
-                        commands.push(Arc::from(command));
-                    }
-                }
-                None => {
-                    let command = AddRole::new(awaiting_role.clone());
-                    commands.push(Arc::from(command));
-                }
+        let ListComparison {
+            extra_other: extra_existing,
+            extra_self: extra_awaiting,
+            same,
+        } = self.items.compare_by_name(&existing_guild.roles);
+
+        for awaiting_role in extra_awaiting.into_iter() {
+            let command = AddRole::new(awaiting_role.clone());
+            commands.push(Arc::from(command));
+        }
+
+        for (awaiting_role, existing_role) in same.into_iter() {
+            let diffs = existing_role.diffs_with(awaiting_role);
+
+            if !diffs.is_empty() {
+                let command = UpdateRole::new(existing_role.clone(), awaiting_role.clone(), diffs);
+                commands.push(Arc::from(command));
             }
         }
 
-        for existing_role in existing_guild.roles.to_list() {
-            if self.items.find_by_name(&existing_role.name).is_none() {
-                self.extra_items_strategy
-                    .handle_extra_role(existing_role, &mut commands);
-            }
+        for existing_role in extra_existing.into_iter() {
+            self.extra_items_strategy
+                .handle_extra_role(existing_role, &mut commands);
         }
 
         commands
